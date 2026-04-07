7dc6cc5e62a5ae1da8f1dc711e179c1461af7158
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2026-04-07 10:18:22 -0400

    s

--- java.js
+++ java.js
@@ -1,1 +1,13 @@
+//Shay VanLandschoot
+//4/7/26
+//array methods practice
 
+const colors = ['red','blue','green', 'purple','yellow'];
+
+
+// const favorit_colors = alert (`I have ${colors.length} favorit colors`);
+// console.log(favorit_colors);
+
+colors.push('pink');
+
+colors.join()
